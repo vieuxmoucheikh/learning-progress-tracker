2c1a7d83025fd11705cdbba99583b6189f237ee4
vieuxmoucheikh <vieuxwillane@gmail.com>
2025-02-27 05:46:32 +0100

    changes

--- src/components/pomodoro/timerWorker.js
+++ src/components/pomodoro/timerWorker.js
@@ -1,13 +1,56 @@
+let interval = null;
+let isRunning = false;
+let lastTickTime = null;
+let remainingTime = 0;
+
 self.onmessage = (e) => {
-    const { command } = e.data;
+    const { command, time } = e.data;
+    
     if (command === 'start') {
-        const interval = setInterval(() => {
-            self.postMessage({ type: 'tick' });
-        }, 1000);
-        self.onmessage = (e) => {
-            if (e.data.command === 'stop') {
-                clearInterval(interval);
+        // Clear any existing interval
+        if (interval) {
+            clearInterval(interval);
+        }
+        
+        isRunning = true;
+        lastTickTime = Date.now();
+        
+        // If time is provided, set the remaining time
+        if (time !== undefined) {
+            remainingTime = time;
+            console.log(`Timer worker: Setting remaining time to ${time} seconds`);
+        }
+        
+        // Start a new interval
+        interval = setInterval(() => {
+            if (isRunning) {
+                // Calculate how many seconds have passed since the last tick
+                const now = Date.now();
+                const elapsedSeconds = Math.floor((now - lastTickTime) / 1000);
+                lastTickTime = now;
+                
+                if (elapsedSeconds > 0) {
+                    // Update the remaining time
+                    remainingTime = Math.max(0, remainingTime - elapsedSeconds);
+                    
+                    // Send a tick message with the updated remaining time
+                    self.postMessage({ 
+                        type: 'tick', 
+                        remainingTime: remainingTime 
+                    });
+                }
             }
-        };
+        }, 1000);
+    } else if (command === 'stop') {
+        isRunning = false;
+        if (interval) {
+            clearInterval(interval);
+            interval = null;
+        }
+    } else if (command === 'pause') {
+        isRunning = false;
+    } else if (command === 'resume') {
+        isRunning = true;
+        lastTickTime = Date.now();
     }
 };
