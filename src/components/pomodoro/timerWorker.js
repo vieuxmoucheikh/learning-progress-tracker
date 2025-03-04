7e53a37876717c7ff39c56187ed5229b7cb69c86
vieuxmoucheikh <vieuxwillane@gmail.com>
2025-03-05 00:24:09 +0100

    deploying

--- src/components/pomodoro/timerWorker.js
+++ src/components/pomodoro/timerWorker.js
@@ -1,36 +1,12 @@
 self.onmessage = (e) => {
-    const { command, time } = e.data;
-    let interval;
-    let startTime;
-    let remainingTime;
-    let isPaused = false;
-
+    const { command } = e.data;
     if (command === 'start') {
-        clearInterval(interval);
-        startTime = Date.now();
-        remainingTime = time || 0;
-        
-        interval = setInterval(() => {
-            if (!isPaused) {
-                self.postMessage({ type: 'tick' });
-            }
+        const interval = setInterval(() => {
+            self.postMessage({ type: 'tick' });
         }, 1000);
-        
         self.onmessage = (e) => {
             if (e.data.command === 'stop') {
                 clearInterval(interval);
-            } else if (e.data.command === 'pause') {
-                isPaused = true;
-            } else if (e.data.command === 'resume') {
-                isPaused = false;
-                startTime = Date.now();
-            } else if (e.data.command === 'sync') {
-                // Sync the timer with the current time
-                const elapsed = Math.floor((Date.now() - e.data.timestamp) / 1000);
-                self.postMessage({ 
-                    type: 'sync', 
-                    elapsed: elapsed 
-                });
             }
         };
     }
