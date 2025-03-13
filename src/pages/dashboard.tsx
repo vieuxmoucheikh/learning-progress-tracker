4e533c02310a00acd61554404c6e2aaffa59c1e2
vieuxmoucheikh <vieuxwillane@gmail.com>
2025-03-14 00:04:12 +0100

    learning cards ui test

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -63,7 +63,7 @@ export default function Dashboard() {
                   onClick={handleSignOut}
                   className="relative inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
-                  Sign Out
+                  Log Out
                 </button>
               </div>
             </div>
